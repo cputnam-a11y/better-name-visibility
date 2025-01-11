ca93d8c30c0f1dd99404c38863b02ddc4a6eace3
cputnam-a11y <chrlsvryptnm@gmail.com>
2025-01-10 23:10:10 -0600

    theoretically it works?

--- src/main/java/com/hoetty/name/visibility/Main.java
+++ src/main/java/com/hoetty/name/visibility/Main.java
@@ -3,32 +3,50 @@ package com.hoetty.name.visibility;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main implements ModInitializer {
+import java.util.function.BooleanSupplier;
 
-	private static KeyBinding toggleNamesKey;
-	public static boolean NamesToggled = true;
-	public static final Logger LOGGER = LoggerFactory.getLogger("name_visibility");
+public class Main implements ModInitializer, ClientTickEvents.EndTick {
 
-	@Override
-	public void onInitialize() {
-		toggleNamesKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.name_visibility.toggleNames", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_BRACKET,"category.name_visibility.keybindcategory"));
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (toggleNamesKey.wasPressed()){
-				if (NamesToggled){
-					NamesToggled = false;
-					client.player.sendMessage(Text.of("Better Name Visibility §cOFF"), true);
-				}else{
-					NamesToggled = true;
-					client.player.sendMessage(Text.of("Better Name Visibility §aON"), true);
-				}
-			}
-		});
-	}
+    private static KeyBinding TOGGLE_NAMES_KEY;
+    public static boolean NAMES_TOGGLED = true;
+
+    @Override
+    public void onInitialize() {
+        TOGGLE_NAMES_KEY = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.name_visibility.toggleNames",
+                        InputUtil.Type.KEYSYM,
+                        InputUtil.UNKNOWN_KEY.getCode(),
+                        "category.name_visibility.keybindcategory"
+                )
+        );
+        ClientTickEvents.END_CLIENT_TICK.register(this);
+    }
+
+    @Override
+    public void onEndTick(MinecraftClient client) {
+        if (!TOGGLE_NAMES_KEY.wasPressed()) {
+            return;
+        }
+        NAMES_TOGGLED = !NAMES_TOGGLED;
+        if (client.player == null)
+            return;
+        Text message = NAMES_TOGGLED
+                       ? Text.translatable("message.name_visibility.enabled")
+                       : Text.translatable("message.name_visibility.disabled");
+        client.player.sendMessage(message, true);
+        runWhileTrue(TOGGLE_NAMES_KEY::wasPressed);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    private static void runWhileTrue(BooleanSupplier supplier) {
+        while (supplier.getAsBoolean());
+    }
 }
--- src/main/java/com/hoetty/name/visibility/mixin/NameTagRenderer.java
+++ src/main/java/com/hoetty/name/visibility/mixin/NameTagRenderer.java
@@ -1,20 +1,37 @@
 package com.hoetty.name.visibility.mixin;
 
-import net.minecraft.client.render.entity.EntityRenderer;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-
 import com.hoetty.name.visibility.Main;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.text.Text;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(EntityRenderer.class)
 public abstract class NameTagRenderer {
-
-    @ModifyArgs(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
-    private void modify(Args args) {
-		if (Main.NamesToggled){
-    		args.set(3, 0xFFFFFFFF);
-		}
-	}
+    @WrapOperation(
+            method = "renderLabelIfPresent",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"
+            )
+    )
+    private int modify(TextRenderer instance, Text text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider vertexConsumers, TextRenderer.TextLayerType layerType, int backgroundColor, int light, Operation<Integer> original) {
+        return original.call(
+                instance, text,
+                x, y,
+                Main.NAMES_TOGGLED
+                ? 0xFFFFFFFF
+                : color,
+                shadow,
+                matrix, vertexConsumers,
+                layerType, backgroundColor,
+                light
+        );
+    }
 }
